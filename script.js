ff128ff18235d84850daca8e200047c97bc1bd2c
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-08-15 23:48:43 +1000

    arrays added for number, special characters and letters

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  // 
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+const specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '\\', '@', "[", ']', '^', '_', '`', '{', '|', '}', '~'];
+
+const lettersOfAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+const numbersZeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
