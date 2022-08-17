94a6073fb85891ed08345b54f72644781bc07c00
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-08-17 22:00:59 +1000

    user prompts added to check password character requirements

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+
+// Arrays for computer to choose from
+
+const specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '\\', '@', "[", ']', '^', '_', '`', '{', '|', '}', '~'];
+
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+const numbersZeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
+
 function generatePassword() {
-  // 
+
+  var lengthOfPassword = window.prompt("How many characters would you like your password to contain? (min 8, max 128)");
+  if (!lengthOfPassword) {
+    return;
+  }
+
+  var lowerCaseResponse = window.confirm("Does your password require a lower-case letter?");
+  
+
+  var upperCaseResponse = window.confirm("Does your password require an upper-case letter?");
+
+  var specialCharResponse = window.confirm("Does your password require a special character?");
+
 }
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -15,13 +42,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 
-const specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '\\', '@', "[", ']', '^', '_', '`', '{', '|', '}', '~'];
-
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-const numbersZeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
