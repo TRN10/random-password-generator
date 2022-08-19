be22fb760d284322af619dc0f2c52e2ac18e7064
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-08-19 23:34:48 +1000

    README updated

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+
+// Arrays for computer to choose from
+
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '\\', '@', "[", ']', '^', '_', '`', '{', '|', '}', '~'];
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numbersZeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var combinedCharacters = [];
+
+var finalPassword = []
+
+
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+
+// Function
+
+function generatePassword() {
+
+  var lengthOfPassword = parseInt(window.prompt("How many characters would you like your password to contain? (minimum 8, maximum 128)"));
+  
+
+  if (Number.isNaN(lengthOfPassword)) {
+    window.alert("Please enter a number")
+    return null;
+  }
+
+  if (lengthOfPassword <= 7) {
+    window.alert("Password must be at least 8 characters")
+    return;
+  }
+
+  if (lengthOfPassword >= 129) {
+    window.alert("Password must be no more than 128 characters")
+    return;
+  }
+
+ 
+  
+  // add lower case array to combinedCharacters array if selected by user
+
+  var lowerCaseResponse = window.confirm("Does your password require a lower-case letter?");
+
+  if(lowerCaseResponse) {
+    combinedCharacters = combinedCharacters.concat(lowerCase);
+  }
+
+
+  // add upper case array to combinedCharacters array if selected by user
+
+  var upperCaseResponse = window.confirm("Does your password require an upper-case letter?");
+
+  if(upperCaseResponse) {
+    combinedCharacters = combinedCharacters.concat(upperCase);
+  }
+
+  // add specialCharacters array to combinedCharacters array if selected by user
+
+  var specialCharResponse = window.confirm("Does your password require a special character?");
+
+  if(specialCharResponse) {
+    combinedCharacters = combinedCharacters.concat(specialCharacters);
+  }
+
+  // add numbersZeroToNine array to combinedCharacters array if selected by user
+
+  var numberResponse = window.confirm("Does your password require a number?");
+
+  if(numberResponse) {
+    combinedCharacters = combinedCharacters.concat(numbersZeroToNine);
+  }
+
+  // randomly select from combinedCharacters times by the password length specified by user input
+
+ for (let i = 0; i < lengthOfPassword; i++) {
+ 
+  var index = Math.floor(Math.random() * combinedCharacters.length);
+  var combinedCharactersRandom = combinedCharacters[index];
+  finalPassword.push(combinedCharactersRandom)
+}
+ 
+return finalPassword.join("");
+
+}
+
+
+
+// Write password to the #password input
+
+function writePassword() {
+  finalPassword = []
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
